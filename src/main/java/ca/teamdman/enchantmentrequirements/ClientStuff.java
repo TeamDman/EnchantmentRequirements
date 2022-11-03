5d9376e04a82643476c1df8e7c549b11a6b61399
TeamDman <TeamDman9201@gmail.com>
2022-11-03 12:39:11 -0400

    fix crashing on server

---
+++ src/main/java/ca/teamdman/enchantmentrequirements/ClientStuff.java
@@ -0,0 +1,143 @@
+package ca.teamdman.enchantmentrequirements;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.inventory.EnchantmentScreen;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.BookItem;
+import net.minecraft.world.item.EnchantedBookItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EnchantmentTableBlock;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = EnchantmentRequirements.MODID)
+public class ClientStuff {
+	private static long lastCheck  = 0;
+	private static int  tablePower = 0;
+
+	@SubscribeEvent
+	public static void onTooltip(ItemTooltipEvent event) {
+		var stack = event.getItemStack();
+		var level = Minecraft.getInstance().level;
+		var tip   = event.getToolTip();
+		var pos   = Minecraft.getInstance().hitResult == null ? BlockPos.ZERO : new BlockPos(Minecraft.getInstance().hitResult.getLocation());
+		if (stack.isEmpty() || level == null || pos.equals(BlockPos.ZERO)) return;
+		handleBlockPower(event, stack, level, pos, tip);
+		handleBookLevel(event, stack, level, pos, tip);
+		handleItemPower(event, stack, level, pos, tip);
+		handleTablePower(event, stack, level, pos, tip);
+	}
+
+	private static void handleItemPower(ItemTooltipEvent event, ItemStack stack, ClientLevel level, BlockPos pos, List<Component> tip) {
+		int enchantValue = stack.getItemEnchantability();
+		if (enchantValue == 0) return;
+		tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.value", enchantValue));
+	}
+
+	private static void handleBlockPower(ItemTooltipEvent event, ItemStack stack, ClientLevel level, BlockPos pos, List<Component> tip) {
+		if (!(stack.getItem() instanceof BlockItem blockItem)) return;
+		var power = blockItem.getBlock()
+				.defaultBlockState()
+				.getEnchantPowerBonus(level, pos);
+		if (power > 0) {
+			tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.power", power));
+		}
+	}
+
+	private static void handleTablePower(ItemTooltipEvent event, ItemStack stack, Level level, BlockPos pos, List<Component> tip) {
+		if (!(Minecraft.getInstance().screen instanceof EnchantmentScreen es)) return;
+		recalculateTablePower(level, pos);
+		tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.table_power", tablePower).withStyle(ChatFormatting.DARK_AQUA));
+		int itemPower = stack.getItemEnchantability();
+		if (itemPower == 0) return;
+		var pPower = Math.min(tablePower, 15);
+		/**
+		 * Do the math from {@link EnchantmentHelper#getEnchantmentCost(RandomSource, int, int, ItemStack)}
+		 */
+		int jLower     = 1 + (pPower >> 1);
+		int jUpper     = 8 + (pPower >> 1) + pPower;
+		int cost0Lower = Math.max(jLower / 3, 1);
+		int cost0Upper = Math.max(jUpper / 3, 1);
+		int cost1Lower = Math.max(jLower * 2 / 3 + 1, 2);
+		int cost1Upper = Math.max(jUpper * 2 / 3 + 1, 2);
+		int cost2Lower = Math.max(Math.max(jLower, pPower * 2), 3);
+		int cost2Upper = Math.max(Math.max(jUpper, pPower * 2), 3);
+		tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.cost_0", cost0Lower, cost0Upper).withStyle(ChatFormatting.DARK_AQUA));
+		tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.cost_1", cost1Lower, cost1Upper).withStyle(ChatFormatting.DARK_AQUA));
+		tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.cost_2", cost2Lower, cost2Upper).withStyle(ChatFormatting.DARK_AQUA));
+	}
+
+	private static void recalculateTablePower(Level level, BlockPos pos) {
+		if (System.currentTimeMillis() <= lastCheck + 1000) return;
+		if (level == null) {
+			tablePower = -1;
+		} else {
+			tablePower = 0;
+			for (BlockPos blockpos : EnchantmentTableBlock.BOOKSHELF_OFFSETS) {
+				if (EnchantmentTableBlock.isValidBookShelf(level, pos, blockpos)) {
+					tablePower += level.getBlockState(pos.offset(blockpos))
+							.getEnchantPowerBonus(level, pos.offset(blockpos));
+				}
+			}
+		}
+		lastCheck = System.currentTimeMillis();
+	}
+
+	private static void handleBookLevel(ItemTooltipEvent event, ItemStack stack, ClientLevel level, BlockPos pos, List<Component> tip) {
+		if (!(stack.getItem() instanceof EnchantedBookItem)) return;
+		var enchants = EnchantmentHelper.getEnchantments(stack);
+		if (enchants.size() != 1) return;
+		var entry = enchants.entrySet()
+				.iterator()
+				.next();
+		var ench      = entry.getKey();
+		var enchLevel = entry.getValue();
+		var minCost   = ench.getMinCost(enchLevel);
+		var maxCost   = ench.getMaxCost(enchLevel);
+		tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.cost", minCost, maxCost));
+
+		// if we have an enchanting table open
+		// determine if the enchantment on the book could be rolled for the current item
+		if (!(Minecraft.getInstance().screen instanceof EnchantmentScreen es)) return;
+		var enchanting = es.getMenu().slots.get(0)
+				.getItem();
+		if (enchanting.isEmpty()) return;
+		var itemPower = enchanting.getItemEnchantability();
+		/**
+		 * Do the math from {@link EnchantmentHelper#getEnchantmentCost(RandomSource, int, int, ItemStack)}
+		 */
+		if (itemPower <= 0) return;
+		int pPower     = Math.min(tablePower, 15);
+		int jLower     = 1 + (pPower >> 1);
+		int jUpper     = 8 + (pPower >> 1) + pPower;
+		int cost0Lower = Math.max(jLower / 3, 1);
+		int cost2Upper = Math.max(Math.max(jUpper, pPower * 2), 3);
+		/**
+		 * Do the math from {@link EnchantmentHelper#selectEnchantment(RandomSource, ItemStack, int, boolean)}
+		 */
+		int   pLevelLower = cost0Lower + 1;
+		int   pLevelUpper = cost2Upper + 1 + itemPower / 2;
+		float fLower      = -0.15F;
+		float fUpper      = 0.15F;
+		pLevelLower = Mth.clamp(Math.round(pLevelLower + pLevelLower * fLower), 1, Integer.MAX_VALUE);
+		pLevelUpper = Mth.clamp(Math.round(pLevelUpper + pLevelUpper * fUpper), 1, Integer.MAX_VALUE);
+		tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.true_cost", pLevelLower, pLevelUpper).withStyle(ChatFormatting.DARK_AQUA));
+
+		if (!ench.canEnchant(enchanting) && !((enchanting.getItem() instanceof BookItem) && ench.isAllowedOnBooks())) {
+			tip.add(new TranslatableComponent("tooltip.enchantmentrequirements.cant_enchant").withStyle(ChatFormatting.RED));
+		}
+	}
+
+}
